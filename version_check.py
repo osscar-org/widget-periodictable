7ba94a528466c9e44b389429b08dce455af5917d
Dou <dou.du.epfl@gmail.com>
2020-03-18 14:20:10 +0100

    Make the workflow with version check

---
+++ version_check.py
@@ -0,0 +1,21 @@
+from setupbase import get_version  
+from os.path import join as pjoin 
+import json 
+
+name = 'widget_periodictable'
+
+version_py = get_version(pjoin(name, '_version.py'))  
+print(version_py)
+
+
+with open('package.json') as json_file:
+    data = json.load(json_file)
+
+version_npm = data['version']
+
+if version_py != version_npm :
+    raise ValueError('The version number are NOT equal')
+else:
+    print('Check fine for the version number')
+
+
