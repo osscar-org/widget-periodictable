c21a4e0f028caafe61b245acdb1c875e29508e04
Dou <dou.du.epfl@gmail.com>
2020-04-10 03:26:04 +0200

    Fix the bug for the disabled

--- src/widget.ts
+++ src/widget.ts
@@ -9,13 +9,13 @@ import {
   MODULE_NAME, MODULE_VERSION
 } from './version';
 
-import * as _ from 'underscore'; 
+import * as _ from 'underscore';
 
 // Import the CSS
-import '../css/ptable.css'; 
+import '../css/ptable.css';
 
 // List of lists of elements, used to render the periodic table
-// Only values accepted: 
+// Only values accepted:
 // - strings (should be valid elements, not checked);
 // - empty strings (empty space, nothing rendered);
 // - '*' character (printed as a disabled element).
@@ -31,7 +31,7 @@ var elementTable: string[][] = [
     ["Fr", "Ra", "*", "Rf", "Db", "Sg", "Bh", "Hs", "Mt","Ds", "Rg", "Cn", "Nh", "Fi", "Mc", "Lv", "Ts", "Og"],
     ["", "", "", "", "", "", "", "", "","", "", "", "", "", "", "", "", ""],
     ["", "", "*", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu","Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu"],
-    ["", "", "*", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am","Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"]    
+    ["", "", "*", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am","Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"]
 ];
 
 // Flat list of elements, used for validation and cleaning up of the
@@ -51,7 +51,7 @@ export
 class MCPTableModel extends DOMWidgetModel {
   defaults() {
     return {...super.defaults(),
-      _model_name: MCPTableModel.model_name, 
+      _model_name: MCPTableModel.model_name,
       _model_module: MCPTableModel.model_module,
       _model_module_version: MCPTableModel.model_module_version,
       _view_name: MCPTableModel.view_name,
@@ -79,7 +79,7 @@ export
 class MCPTableView extends DOMWidgetView {
     // TODO: move template to external file to make it more readable, see
     // http://codebeerstartups.com/2012/12/how-to-improve-templates-in-backbone-js-learning-backbone-js/
-    tableTemplate =  _.template( '<% for (let elementRow of elementTable) { print("<div class=\'periodic-table-row\'>"); for (let elementName of elementRow) { if ( (elementName === "") || (elementName == "*" ) ) { %>' + '  <span class="periodic-table-empty noselect"><%= elementName %></span>' + '<% } else { %>' + '  <span class="<% if (disabledElements.includes(elementName)) { print(" periodic-table-disabled"); } else { print(" periodic-table-entry"); }%> noselect element-<%= elementName %><% if (selectedElements.includes(elementName) && (! disabledElements.includes(elementName)) ) { print(" elementOn"); } %>"><% print(displayNamesReplacements[elementName] || elementName); %></span>' + '<% } }; print("</div>"); } %>'); 
+    tableTemplate =  _.template( '<% for (let elementRow of elementTable) { print("<div class=\'periodic-table-row\'>"); for (let elementName of elementRow) { if ( (elementName === "") || (elementName == "*" ) ) { %>' + '  <span class="periodic-table-empty noselect"><%= elementName %></span>' + '<% } else { %>' + '  <span class="<% if (disabledElements.includes(elementName)) { print(" periodic-table-disabled"); } else { print(" periodic-table-entry"); }%> noselect element-<%= elementName %><% if (selectedElements.includes(elementName) && (! disabledElements.includes(elementName)) ) { print(" elementOn"); } %>"><% print(displayNamesReplacements[elementName] || elementName); %></span>' + '<% } }; print("</div>"); } %>');
 
     render() {
         // I render the widget
@@ -88,39 +88,48 @@ class MCPTableView extends DOMWidgetView {
         this.model.on('change:selected_elements', this.rerenderScratch, this);
         this.model.on('change:disabled_elements', this.rerenderScratch, this);
         this.model.on('change:display_names_replacements', this.rerenderScratch, this);
-    } 
+    }
 
     events(): {[e: string]: string} {
-        return {"click .periodic-table-entry": "toggleElement"};
+        return {"click .periodic-table-entry": "toggleElement", "click .periodic-table-disabled": "toggleElement"};
 }
 
     toggleElement(event: any) {
-        let classNames: object = _.map(event.target.classList, function(a){return a});
-        let tempName: string = _.chain(classNames)
-            .last()
+        let classNames: string[] = _.map(event.target.classList, function(a: string){return a});
+        let elementName: string = _.chain(classNames)
+            .filter(function(a: any){return a.startsWith('element-')})
+            .map(function(a: any){return a.slice("element-".length);})
+            .first()
             .value();
-        var elementName = tempName.slice(8);
 
         var isOn = _.includes(classNames, 'elementOn');
         var isDisabled = _.includes(classNames, "periodic-table-disabled");
         // If this button is disabled, do not do anything
-        // (Actually, this function should not be triggered if the button 
+        // (Actually, this function should not be triggered if the button
         // is disabled, this is just a safety measure)
-        if (isDisabled) return;
+        // if (isDisabled) return;
 
         // Check if we understood which element we are
         if (typeof elementName !== 'undefined') {
             var currentList = this.model.get('selected_elements');
+            var currentDisabledList = this.model.get('disabled_elements');
             // NOTE! it is essential to duplicate the list,
             // otherwise the value will not be updated.
             var newList = currentList.slice();
+            var newDisabledList = currentDisabledList.slice();
 
             if (isOn) {
                 // remove the element from the selected_elements
                 newList = _.without(newList, elementName);
+                newDisabledList.push(elementName);
                 // Swap CSS state
                 event.target.classList.remove('elementOn');
-            } else {
+            }
+            else if (isDisabled) {
+                newDisabledList = _.without(newDisabledList, elementName);
+                event.target.classList.remove('periodic-table-disabled');
+            }
+            else {
                 // add the element from the selected_elements
                 newList.push(elementName);
                 // Swap CSS state
@@ -129,10 +138,11 @@ class MCPTableView extends DOMWidgetView {
 
             // Update the model (send back data to python)
             this.model.set('selected_elements', newList);
+            this.model.set('disabled_elements', newDisabledList);
             this.touch();
         }
     }
-    
+
     rerenderScratch() {
 //         Re-render full widget when the list of selected elements
 //         changed from python
@@ -160,11 +170,11 @@ class MCPTableView extends DOMWidgetView {
             this.model.set('selected_elements', newSelectedElements);
             this.touch();
         }
-        
+
 //         Render the full widget using the template
         this.el.innerHTML = '<div class="periodic-table-body">' +
             this.tableTemplate({
-                elementTable: elementTable, 
+                elementTable: elementTable,
                 displayNamesReplacements: this.model.get('display_names_replacements'),
                 selectedElements: newSelectedElements,
                 disabledElements: disabledElements
