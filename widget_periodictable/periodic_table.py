d7bf982cb6c295bc9bbc46f0e2dcdaee0ded688c
Dou <dou.du.epfl@gmail.com>
2020-05-23 16:39:32 +0200

    Use dictionary to fix issue #4

--- widget_periodictable/periodic_table.py
+++ widget_periodictable/periodic_table.py
@@ -13,18 +13,6 @@ from traitlets import Unicode, Int, List, Dict, observe, validate, TraitError, D
 from ._frontend import module_name, module_version
 from copy import deepcopy
 
-allElements = [
-    "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg",
-    "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe",
-    "Co","Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y",
-    "Zr", "Nb", "Mo",  "Tc", "Ru", "Rh","Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te",
-    "I", "Xe", "Cs", "Ba", "Hf", "Ta", "W", "Re", "Os", "Ir","Pt", "Au", "Hg", "Tl",
-    "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Rf", "Db", "Sg", "Bh", "Hs",
-    "Mt","Ds", "Rg", "Cn", "Nh", "Fi", "Mc", "Lv", "Ts", "Og", "La", "Ce", "Pr",
-    "Nd", "Pm", "Sm", "Eu","Gd",  "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu","Ac",
-    "Th", "Pa", "U", "Np", "Pu", "Am","Cm", "Bk",  "Cf", "Es", "Fm", "Md", "No", "Lr"
-];
-
 class PTableWidget(DOMWidget):
     """TODO: Add docstring here
     """
@@ -41,6 +29,17 @@ class PTableWidget(DOMWidget):
     unselected_color = Unicode('pink').tag(sync=True)
     states = Int(1).tag(sync=True)
     selected_colors = List([]).tag(sync=True)
+    allElements = List([
+        "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg",
+        "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe",
+        "Co","Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y",
+        "Zr", "Nb", "Mo",  "Tc", "Ru", "Rh","Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te",
+        "I", "Xe", "Cs", "Ba", "Hf", "Ta", "W", "Re", "Os", "Ir","Pt", "Au", "Hg", "Tl",
+        "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Rf", "Db", "Sg", "Bh", "Hs",
+        "Mt","Ds", "Rg", "Cn", "Nh", "Fi", "Mc", "Lv", "Ts", "Og", "La", "Ce", "Pr",
+        "Nd", "Pm", "Sm", "Eu","Gd",  "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu","Ac",
+        "Th", "Pa", "U", "Np", "Pu", "Am","Cm", "Bk",  "Cf", "Es", "Fm", "Md", "No", "Lr"
+    ]);
 
     def __init__(self, states = 1, selected_elements = {}, disabled_color = 'gray', unselected_color = 'pink', selected_colors = ["#a6cee3", "#b2df8a", "#fdbf6f", "#6a3d9a", "#b15928", "#e31a1c", "#1f78b4", "#33a02c", "#ff7f00", "#cab2d6", "#ffff99"]):
         super(PTableWidget, self).__init__()
@@ -55,8 +54,9 @@ class PTableWidget(DOMWidget):
             self.selected_colors = selected_colors + additional_colors * (1 + (states - len(selected_colors)) // len(additional_colors))
             self.selected_colors = self.selected_colors[:states]
 
+
     def set_element_state(self, elementName, state):
-        if elementName not in allElements:
+        if elementName not in self.allElements:
             raise TraitError('Element not found')
         if state not in range(self.states):
             raise TraitError('State value is wrong')
@@ -67,7 +67,7 @@ class PTableWidget(DOMWidget):
     @validate('selected_elements')
     def _selectedElements_change(self, proposal):
         for x, y in proposal['value'].items():
-            if x not in allElements:
+            if x not in self.allElements:
                 raise TraitError('Element not found')
             if not isinstance(y, int) or y not in range(self.states):
                 raise TraitError('State value is wrong')
