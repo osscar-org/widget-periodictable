0a7f1cb9dd8f2d9da0632c8920aab82c389763a5
Dou <dou.du.epfl@gmail.com>
2021-09-09 23:56:54 +0200

    update the index.spec.ts for the npm test

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -6,20 +6,20 @@
 
 import { createTestModel } from './utils';
 
-import { ExampleModel } from '..';
+import { MCPTableModel } from '..';
 
 describe('Example', () => {
-  describe('ExampleModel', () => {
+  describe('MCPTableModel', () => {
     it('should be createable', () => {
-      const model = createTestModel(ExampleModel);
-      expect(model).toBeInstanceOf(ExampleModel);
+      const model = createTestModel(MCPTableModel);
+      expect(model).toBeInstanceOf(MCPTableModel);
       expect(model.get('value')).toEqual('Hello World');
     });
 
     it('should be createable with a value', () => {
       const state = { value: 'Foo Bar!' };
-      const model = createTestModel(ExampleModel, state);
-      expect(model).toBeInstanceOf(ExampleModel);
+      const model = createTestModel(MCPTableModel, state);
+      expect(model).toBeInstanceOf(MCPTableModel);
       expect(model.get('value')).toEqual('Foo Bar!');
     });
   });
