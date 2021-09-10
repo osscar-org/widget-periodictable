b1173118071505ab2fc9e25c3181ef7bfef95798
Dou <dou.du.epfl@gmail.com>
2021-09-10 03:41:20 +0200

    publish v3.0.0 at PyPi

--- widget_periodictable/_version.py
+++ widget_periodictable/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Dou Du.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (3, 0, 0, 'dev')
+version_info = (3, 0, 0)
 __version__ = ".".join(map(str, version_info))
