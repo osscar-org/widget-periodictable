1e2b547b6aa5e9577c5e1b5a5f2c8315524791c9
Dou Du <dou.du@epfl.ch>
2023-05-26 10:37:26 +0200

    release v3.1.1

--- widget_periodictable/_version.py
+++ widget_periodictable/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Dou Du,Casper Welzel Andersen.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (3, 1, 0)
+version_info = (3, 1, 1)
 __version__ = ".".join(map(str, version_info))
