83595edfeb0b6dd46f3c8e8fe6db0105b81287c0
Dou Du <dou.du@icloud.com>
2024-03-10 15:41:14 +0100

    remove all cookiecutter widget files

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Dou Du,Casper Welzel Andersen
-// Distributed under the terms of the Modified BSD License.
-
-export * from './version';
-export * from './widget';
--- widget_periodictable/_version.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Copyright (c) Dou Du,Casper Welzel Andersen.
-# Distributed under the terms of the Modified BSD License.
-
-version_info = (3, 1, 1)
-__version__ = ".".join(map(str, version_info))
